703166df437b06548c803c433e06d50fa420b106
Anna Shulyak <ourmac@Ours-iMac.fios-router.home>
2022-10-12 18:03:26 -0400

    commit tree 661d5161ce618d8b3f7a9d2ecb11480e462e7db8

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.create([{first_name: "Anya", last_name: 'Shu', grade: 12},
+{first_name: "Lenya", last_name: 'Ozerov', grade: 3},
+{first_name: "Lera", last_name: 'Volga', grade: 10}])
